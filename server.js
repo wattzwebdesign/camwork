f9cfac16729f972bba1653f97d9a07bd72f91d9c
donwattz1459 <mcguinn.donald@gmail.com>
2025-10-07 16:48:38 -0400

    updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
